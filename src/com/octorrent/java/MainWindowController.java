c1514df3608753946349208611b7628a9393c69e
DrBrad <brad@bradeagle.com>
2024-11-01 11:30:14 -0600

    List view

--- src/com/octorrent/java/MainWindowController.java
+++ src/com/octorrent/java/MainWindowController.java
@@ -3,27 +3,70 @@ package com.octorrent.java;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class MainWindowController {
 
     @FXML
-    private AnchorPane all_tab_layout;
+    private ScrollPane allTabLayout;
 
     @FXML
     public void initialize(){
-        // Create the ListView dynamically
         ListView<String> listView = new ListView<>();
-        listView.getItems().addAll("Item 1", "Item 2", "Item 3");
+        listView.getItems().addAll("Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 1",
+                "Item 2",
+                "Item 3");
 
-        // Customize the ListView if needed
-        listView.setPrefHeight(200);
-        listView.setPrefWidth(150);
-
-        // Add the ListView to the VBox
-        all_tab_layout.getChildren().add(listView);
+        allTabLayout.setContent(listView);
     }
 
     @FXML
---
+++ src/com/octorrent/java/handlers/Torrent.java
@@ -0,0 +1,45 @@
+package com.octorrent.java.handlers;
+
+public class Torrent {
+
+    private String title;
+    private long started;
+    private int peers, seeders, completed, total;
+
+    public Torrent(String title){
+        this.title = title;
+        started = System.currentTimeMillis();
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public int getPeers(){
+        return peers;
+    }
+
+    public int getSeeders(){
+        return seeders;
+    }
+
+    public int getProgress(){
+        return completed;
+    }
+
+    public int getTotal(){
+        return total;
+    }
+
+    public int getLeft(){
+        return total-completed;
+    }
+
+    public long getStarted(){
+        return started;
+    }
+}
