0ffd5e8835c8ae713d4310af4136a439dff32716
DrBrad <brad@bradeagle.com>
2024-11-01 15:54:55 -0600

    Create row

--- src/com/octorrent/java/MainWindowController.java
+++ src/com/octorrent/java/MainWindowController.java
@@ -8,9 +8,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -31,29 +29,36 @@ public class MainWindowController {
 
     private Node createRow(Torrent torrent){
         HBox hbox = new HBox();
+        hbox.getStyleClass().add("hbox");
 
         ImageView icon = new ImageView();
-        hbox.getChildren().add(icon);
+        icon.setFitWidth(50);
+        icon.setFitHeight(50);
+        //hbox.getChildren().add(icon);
 
         VBox vbox = new VBox();
+        vbox.getStyleClass().add("vbox");
+        vbox.setFillWidth(true);
+        HBox.setHgrow(vbox, Priority.ALWAYS);
 
         Label title = new Label();
+        title.setMaxWidth(Double.MAX_VALUE);
         title.setText(torrent.getTitle());
-        vbox.getChildren().add(title);
 
         ProgressBar progress = new ProgressBar();
+        progress.setMaxWidth(Double.MAX_VALUE);
         progress.setProgress(0.5);
-        vbox.getChildren().add(progress);
 
         Label status = new Label();
+        status.setMaxWidth(Double.MAX_VALUE);
         status.setText("adasdasdsad");
-        vbox.getChildren().add(status);
 
         Label description = new Label();
+        description.setMaxWidth(Double.MAX_VALUE);
         description.setText("adasdasdsad");
-        vbox.getChildren().add(description);
 
-        hbox.getChildren().add(vbox);
+        vbox.getChildren().addAll(title, progress, status, description);
+        hbox.getChildren().addAll(icon, vbox);
 
         return hbox;
     }
