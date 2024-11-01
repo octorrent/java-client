4c3773c9fea23da43b97b87a646afdedab97221c
DrBrad <brad@bradeagle.com>
2024-11-01 11:07:21 -0600

    List view

--- src/com/octorrent/java/MainWindowController.java
+++ src/com/octorrent/java/MainWindowController.java
@@ -2,13 +2,29 @@ package com.octorrent.java;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
 
 import java.awt.event.ActionEvent;
 
 public class MainWindowController {
 
     @FXML
-    private Label label;  // Connects with the Label in the FXML file
+    private AnchorPane all_tab_layout;
+
+    @FXML
+    public void initialize(){
+        // Create the ListView dynamically
+        ListView<String> listView = new ListView<>();
+        listView.getItems().addAll("Item 1", "Item 2", "Item 3");
+
+        // Customize the ListView if needed
+        listView.setPrefHeight(200);
+        listView.setPrefWidth(150);
+
+        // Add the ListView to the VBox
+        all_tab_layout.getChildren().add(listView);
+    }
 
     @FXML
     private void onOpenTorrentAction(){
