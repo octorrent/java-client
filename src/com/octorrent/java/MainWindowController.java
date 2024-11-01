efc96e65b750ed59164db55a2a12623e7a9185bf
DrBrad <brad@bradeagle.com>
2024-11-01 13:27:51 -0600

    List view

--- src/com/octorrent/java/MainWindowController.java
+++ src/com/octorrent/java/MainWindowController.java
@@ -1,10 +1,15 @@
 package com.octorrent.java;
 
+import com.octorrent.java.handlers.Torrent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -16,57 +21,36 @@ public class MainWindowController {
 
     @FXML
     public void initialize(){
-        ListView<String> listView = new ListView<>();
-        listView.getItems().addAll("Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 1",
-                "Item 2",
-                "Item 3");
+        ListView<Node> listView = new ListView<>();
+        listView.getItems().add(createRow(new Torrent("asd")));
+
 
         allTabLayout.setContent(listView);
+    }
+
+    private Node createRow(Torrent torrent){
+        HBox hbox = new HBox();
+
+        ImageView icon = new ImageView();
+        hbox.getChildren().add(icon);
+
+        VBox vbox = new VBox();
+
+        Label title = new Label();
+        title.setText("TEST");
+        vbox.getChildren().add(title);
+
+        Label status = new Label();
+        status.setText("adasdasdsad");
+        vbox.getChildren().add(status);
+
+        Label description = new Label();
+        description.setText("adasdasdsad");
+        vbox.getChildren().add(description);
+
+        hbox.getChildren().add(vbox);
+
+        return hbox;
     }
 
     @FXML
