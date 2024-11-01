36ae276b3128f0284b003b072a4092baa6f022e5
DrBrad <brad@bradeagle.com>
2024-11-01 13:36:25 -0600

    List view

--- src/com/octorrent/java/MainWindowController.java
+++ src/com/octorrent/java/MainWindowController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -37,8 +38,12 @@ public class MainWindowController {
         VBox vbox = new VBox();
 
         Label title = new Label();
-        title.setText("TEST");
+        title.setText(torrent.getTitle());
         vbox.getChildren().add(title);
+
+        ProgressBar progress = new ProgressBar();
+        progress.setProgress(0.5);
+        vbox.getChildren().add(progress);
 
         Label status = new Label();
         status.setText("adasdasdsad");
